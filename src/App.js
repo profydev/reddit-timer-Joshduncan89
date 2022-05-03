38f5f2361d734917f5704c3460e11357675f7ea3
Joshduncan89 <51183094+Joshduncan89@users.noreply.github.com>
2022-05-03 16:10:21 -0400

    Added routes and global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Normalize from 'react-normalize';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
-  return <div>App Placeholder</div>;
+  return (
+    <>
+      <Normalize />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
