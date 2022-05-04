6eb71e8ef9e2877a93e021e72f2add2c461599bf
Joshduncan89 <51183094+Joshduncan89@users.noreply.github.com>
2022-05-04 18:14:07 -0400

    initial commit (#1)
    * installed fonts and format settings
    
    * Added routes and global styles
    
    * Introduce end-of-line final normalization if needed
    
    * Added favicon and changed description and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Normalize from 'react-normalize';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <Normalize />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
