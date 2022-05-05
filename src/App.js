a9b7d3a5d93c8e2a5e8dcce3632621556651b651
Joshduncan89 <51183094+Joshduncan89@users.noreply.github.com>
2022-05-05 11:28:00 -0400

    Added header logo and links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Normalize from 'react-normalize';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header';
 import Home from './pages/Home';
 import Search from './pages/Search';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Normalize />
+      <Header />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
